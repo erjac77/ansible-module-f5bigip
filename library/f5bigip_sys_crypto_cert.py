a238f020f9d6244103c7d6dfa5e1f4d94b29de3b
erjac77 <erjac77@gmail.com>
2017-01-10 16:07:20 -0500

    Add basic Crypto Key and Cert (SYS) support

---
+++ library/f5bigip_sys_crypto_cert.py
@@ -0,0 +1,333 @@
+#!/usr/bin/python
+#
+# Copyright 2016, Eric Jacob <erjac77@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: f5bigip_sys_crypto_cert
+short_description: BIG-IP sys crypto cert module
+description:
+    - Manage cryptographic certificates on the BIG-IP system.
+version_added: "1.0"
+author:
+    - "Eric Jacob, @erjac77"
+notes:
+    - Requires BIG-IP software version >= 11.6
+requirements:
+    - f5-sdk
+options:
+    city:
+        description:
+            - Specifies the x509 city field to be used in creation of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    command:
+        description:
+            - Specifies the command to execute.
+        required: false
+        default: null
+        choices: ['install']
+        aliases: []
+        version_added: 1.0
+    common_name:
+        description:
+            - Specifies the x509 common-name to be used in creation of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    consumer:
+        description:
+            - Specifies the system component by which a key and/or associated cryptographic file will be consumed.
+        required: false
+        default: ltm
+        choices: ['enterprise-manager', 'iquery', 'iquery-big3d', 'ltm', 'webserver']
+        aliases: []
+        version_added: 1.0
+    country:
+        description:
+            - Specifies the x509 country to be used in creation of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    email_address:
+        description:
+            - Specifies the x509 email-address to be used in creation of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    from_editor:
+        description:
+            - Specifies that the key should be obtained from a text editor session.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    from_local_file:
+        description:
+            - Specifies a local file path from which a key is to be copied.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    from_url:
+        description:
+            - Specifies a URI which is to be used to obtain a key for import into the configuration of the system.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    key:
+        description:
+            - Specifies a key from which a certificate should be generated when using the create command.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    lifetime:
+        description:
+            - Specifies the certificate life time to be used in creation of the certificate associated with the given key.
+        required: false
+        default: 365
+        choices: []
+        aliases: []
+        version_added: 1.0
+    organization:
+        description:
+            - Specifies the x509 organization to be used in creation of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    ou:
+        description:
+            - Specifies the x509 organizational unit to be used in creation of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    name:
+        description:
+            - Specifies unique name for the component.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    no_overwrite:
+        description:
+            - Specifies option of not overwriting a key if it is in the scope.
+        required: false
+        default: true
+        choices: [true, false]
+        aliases: []
+        version_added: 1.0
+    partition:
+        description:
+            - Displays the administrative partition in which the component object resides.
+        required: false
+        default: Common
+        choices: []
+        aliases: []
+        version_added: 1.0
+    state:
+        description:
+            - Specifies the state of the component on the BIG-IP system.
+        required: false
+        default: present
+        choices: ['absent', 'present']
+        aliases: []
+        version_added: 1.0
+    state_province:
+        description:
+            - Specifies the x509 state or province of the certificate associated with the given key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    subject_alternative_name:
+        description:
+            - Specifies standard X.509 extensions as shown in RFC 2459.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+'''
+
+EXAMPLES = '''
+- name: Install SYS Crypto Cert from local file
+  f5bigip_sys_crypto_cert:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: exemple.localhost.crt
+    from_local_file: /tmp/exemple.localhost.crt
+    state: present
+  delegate_to: localhost
+
+- name: Create SYS Crypto Cert
+  f5bigip_sys_crypto_cert:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: exemple.localhost.crt
+    key: exemple.localhost.key
+    common_name: exemple.localhost
+    city: city
+    state_province: state
+    country: US
+    email_address: 'admin@localhost'
+    organization: My Org
+    ou: My Div
+    state: present
+  delegate_to: localhost
+
+- name: Delete SYS Crypto Cert
+  f5bigip_sys_crypto_cert:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: exemple.localhost.crt
+    state: absent
+  delegate_to: localhost
+'''
+
+from ansible_common_f5bigip.f5bigip import *
+
+BIGIP_SYS_CRYPTO_CERT_ARGS = dict(
+    consumer                    =   dict(type='str', choices=['enterprise-manager', 'iquery', 'iquery-big3d', 'ltm', 'webserver']),
+    # create
+    city                        =   dict(type='str'),
+    common_name                 =   dict(type='str'),
+    country                     =   dict(type='str'),
+    email_address               =   dict(type='str'),
+    key                         =   dict(type='str'),
+    lifetime                    =   dict(type='int'),
+    organization                =   dict(type='str'),
+    ou                          =   dict(type='str'),
+    state_province              =   dict(type='str'),
+    subject_alternative_name    =   dict(type='str'),
+    # install
+    command                     =   dict(type='str', choices=['install']),
+    from_editor                 =   dict(type='str'),
+    from_local_file             =   dict(type='str'),
+    from_url                    =   dict(type='str'),
+    no_overwrite                =   dict(type='bool')
+)
+
+class F5BigIpSysCryptoCert(F5BigIpObject):
+    def _set_crud_methods(self):
+        self.methods = {
+            'create':self.mgmt.tm.sys.crypto.certs.cert.create,
+            'read':self.mgmt.tm.sys.crypto.certs.cert.load,
+            'update':self.mgmt.tm.sys.crypto.certs.cert.update,
+            'delete':self.mgmt.tm.sys.crypto.certs.cert.delete,
+            'exists':self.mgmt.tm.sys.crypto.certs.cert.exists,
+            'exec_cmd':self.mgmt.tm.sys.crypto.certs.exec_cmd
+        }
+
+    def _exists(self):
+        """Check for the existence of the named object on the BIG-IP system."""
+        keys = self.mgmt.tm.sys.crypto.certs.get_collection()
+        for key in keys:
+            name = self.params['name']
+            if key.name == name:
+                return True
+
+        return False
+
+    def _install(self):
+        """Install the key on the BIG-IP system."""
+        name = self.params['name']
+
+        if self.params['fromEditor']:
+            param_set = { 'from-editor': self.params['fromEditor'] }
+        if self.params['fromLocalFile']:
+            param_set = { 'from-local-file': self.params['fromLocalFile'] }
+        if self.params['fromUrl']:
+            param_set = { 'from-url': self.params['fromUrl'] }
+
+        if param_set:
+            param_set.update({ 'name': name })
+            if self.params['consumer']:
+                param_set.update({ 'consumer': self.params['consumer'] })
+            if self.params['noOverwrite']:
+                param_set.update({ 'no-overwrite': self.params['noOverwrite'] })
+
+            # Install the key
+            self.methods['exec_cmd']('install', **param_set)
+        else:
+            raise AnsibleModuleF5BigIpError("Missing required parameter 'from-*' to install the cert.")
+
+        # Make sure it is installed
+        if not self._exists():
+            raise AnsibleModuleF5BigIpError("Failed to create the object.")
+        
+        return True
+
+    def _present(self):
+        has_changed = False
+        
+        if self.params['command'] == 'install':
+            if not self._exists() or (self.params['noOverwrite'] is not None and self.params['noOverwrite'] is False):
+                has_changed = self._install()
+        else:
+            if not self._exists():
+                has_changed = self._create()
+        
+        return has_changed
+
+def main():
+    # Translation list for conflictual params
+    tr = { 'state_province':'state' }
+    
+    module = AnsibleModuleF5BigIpObject(
+        argument_spec=BIGIP_SYS_CRYPTO_CERT_ARGS,
+        supports_check_mode=False,
+        mutually_exclusive=[
+            ['from_editor', 'from_local_file', 'from_url']
+        ]
+    )
+    
+    try:
+        obj = F5BigIpSysCryptoCert(check_mode=module.supports_check_mode, tr=tr, **module.params)
+        result = obj.flush()
+        module.exit_json(**result)
+    except Exception as exc:
+        module.fail_json(msg=str(exc))
+
+from ansible.module_utils.basic import *
+
+if __name__ == '__main__':  
+    main()
---
+++ library/f5bigip_sys_crypto_key.py
@@ -0,0 +1,298 @@
+#!/usr/bin/python
+#
+# Copyright 2016, Eric Jacob <erjac77@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: f5bigip_sys_crypto_key
+short_description: BIG-IP sys crypto key module
+description:
+    - Manage cryptographic keys and related objects on the BIG-IP system.
+version_added: "1.0"
+author:
+    - "Eric Jacob, @erjac77"
+notes:
+    - Requires BIG-IP software version >= 11.6
+requirements:
+    - f5-sdk
+options:
+    challenge_password:
+        description:
+            - Specifies the challenge password to create the certificate request key.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    command:
+        description:
+            - Specifies the command to execute.
+        required: false
+        default: null
+        choices: ['install']
+        aliases: []
+        version_added: 1.0
+    consumer:
+        description:
+            - Specifies the system component by which a key and/or associated cryptographic file will be consumed.
+        required: false
+        default: ltm
+        choices: ['enterprise-manager', 'iquery', 'iquery-big3d', 'ltm', 'webserver']
+        aliases: []
+        version_added: 1.0
+    curve_name:
+        description:
+            - Specifies the curve name to be used in creation of elliptc curve (EC) key.
+        required: false
+        default: prime256v1
+        choices: ['prime256v1', 'secp384r1']
+        aliases: []
+        version_added: 1.0
+    from_editor:
+        description:
+            - Specifies that the key should be obtained from a text editor session.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    from_local_file:
+        description:
+            - Specifies a local file path from which a key is to be copied.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    from_url:
+        description:
+            - Specifies a URI which is to be used to obtain a key for import into the configuration of the system.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    key_size:
+        description:
+            - Specifies the size, in bits, of the key to be generated.
+        required: false
+        default: null
+        choices: ['512', '1024', '2048', '4096']
+        aliases: []
+        version_added: 1.0
+    key_type:
+        description:
+            - Specifies the size, in bits, of the key to be generated.
+        required: false
+        default: null
+        choices: ['dsa-private', 'ec-private', 'rsa-private']
+        aliases: []
+        version_added: 1.0
+    lifetime:
+        description:
+            - Specifies the certificate life time to be used in creation of the certificate associated with the given key.
+        required: false
+        default: 365
+        choices: []
+        aliases: []
+        version_added: 1.0
+    name:
+        description:
+            - Specifies unique name for the component.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    no_overwrite:
+        description:
+            - Specifies option of not overwriting a key if it is in the scope.
+        required: false
+        default: true
+        choices: [true, false]
+        aliases: []
+        version_added: 1.0
+    partition:
+        description:
+            - Displays the administrative partition in which the component object resides.
+        required: false
+        default: Common
+        choices: []
+        aliases: []
+        version_added: 1.0
+    passphrase:
+        description:
+            - Specifies an optional passphrase with which the key has been protected.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    security_type:
+        description:
+            - Specifies the level of security used in storing the key in question.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 1.0
+    state:
+        description:
+            - Specifies the state of the component on the BIG-IP system.
+        required: false
+        default: present
+        choices: ['absent', 'present']
+        aliases: []
+        version_added: 1.0
+'''
+
+EXAMPLES = '''
+- name: Install SYS Crypto Key from local file
+  f5bigip_sys_crypto_key:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: example.localhost.key
+    from_local_file: /tmp/example.localhost.key
+    state: present
+  delegate_to: localhost
+
+- name: Create SYS Crypto Key
+  f5bigip_sys_crypto_key:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: example.localhost.key
+    state: present
+  delegate_to: localhost
+
+- name: Delete SYS Crypto Key
+  f5bigip_sys_crypto_key:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: example.localhost.key
+    state: absent
+  delegate_to: localhost
+'''
+
+from ansible_common_f5bigip.f5bigip import *
+
+BIGIP_SYS_CRYPTO_KEY_ARGS = dict(
+    consumer                    =   dict(type='str', choices=['enterprise-manager', 'iquery', 'iquery-big3d', 'ltm', 'webserver']),
+    # create
+    challenge_password          =   dict(type='str'),
+    curve_name                  =   dict(type='str', choices=['prime256v1', 'secp384r1']),
+    key_size                    =   dict(type='str', choices=['512', '1024', '2048', '4096']),
+    key_type                    =   dict(type='str', choices=['dsa-private', 'ec-private', 'rsa-private']),
+    lifetime                    =   dict(type='int'),
+    passphrase                  =   dict(type='str'),
+    security_type               =   dict(type='str', choices=['fips', 'normal', 'password', 'nethsm']),
+    # install
+    command                     =   dict(type='str', choices=['install']),
+    from_editor                 =   dict(type='str'),
+    from_local_file             =   dict(type='str'),
+    from_url                    =   dict(type='str'),
+    no_overwrite                =   dict(type='bool')
+)
+
+class F5BigIpSysCryptoKey(F5BigIpObject):
+    def _set_crud_methods(self):
+        self.methods = {
+            'create':self.mgmt.tm.sys.crypto.keys.key.create,
+            'read':self.mgmt.tm.sys.crypto.keys.key.load,
+            'update':self.mgmt.tm.sys.crypto.keys.key.update,
+            'delete':self.mgmt.tm.sys.crypto.keys.key.delete,
+            'exists':self.mgmt.tm.sys.crypto.keys.key.exists,
+            'exec_cmd':self.mgmt.tm.sys.crypto.keys.exec_cmd
+        }
+
+    def _exists(self):
+        """Check for the existence of the named object on the BIG-IP system."""
+        keys = self.mgmt.tm.sys.crypto.keys.get_collection()
+        for key in keys:
+            name = self.params['name']
+            if key.name == name:
+                return True
+
+        return False
+
+    def _install(self):
+        """Install the key on the BIG-IP system."""
+        name = self.params['name']
+
+        if self.params['fromEditor']:
+            param_set = { 'from-editor': self.params['fromEditor'] }
+        if self.params['fromLocalFile']:
+            param_set = { 'from-local-file': self.params['fromLocalFile'] }
+        if self.params['fromUrl']:
+            param_set = { 'from-url': self.params['fromUrl'] }
+
+        if param_set:
+            param_set.update({ 'name': name })
+            if self.params['consumer']:
+                param_set.update({ 'consumer': self.params['consumer'] })
+            if self.params['noOverwrite']:
+                param_set.update({ 'no-overwrite': self.params['noOverwrite'] })
+
+            # Install the key
+            self.methods['exec_cmd']('install', **param_set)
+        else:
+            raise AnsibleModuleF5BigIpError("Missing required parameter 'from-*' to install the key.")
+
+        # Make sure it is installed
+        if not self._exists():
+            raise AnsibleModuleF5BigIpError("Failed to create the object.")
+        
+        return True
+
+    def _present(self):
+        has_changed = False
+        
+        if self.params['command'] == 'install':
+            if not self._exists() or (self.params['noOverwrite'] is not None and self.params['noOverwrite'] is False):
+                has_changed = self._install()
+        else:
+            if not self._exists():
+                has_changed = self._create()
+        
+        return has_changed
+
+def main():
+    # Translation list for conflictual params
+    tr = {}
+    
+    module = AnsibleModuleF5BigIpObject(
+        argument_spec=BIGIP_SYS_CRYPTO_KEY_ARGS,
+        supports_check_mode=False,
+        mutually_exclusive=[
+            ['from_editor', 'from_local_file', 'from_url']
+        ]
+    )
+    
+    try:
+        obj = F5BigIpSysCryptoKey(check_mode=module.supports_check_mode, tr=tr, **module.params)
+        result = obj.flush()
+        module.exit_json(**result)
+    except Exception as exc:
+        module.fail_json(msg=str(exc))
+
+from ansible.module_utils.basic import *
+
+if __name__ == '__main__':  
+    main()
