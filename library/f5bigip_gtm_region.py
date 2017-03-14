62486a5e3ad78351fa3d38a2ead6654277bddd4b
erjac77 <erjac77@gmail.com>
2017-03-14 15:00:18 -0400

    Add GTM Region support

---
+++ library/f5bigip_gtm_region.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+#
+# Copyright 2016, Eric Jacob <erjac77@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: f5bigip_gtm_region
+short_description: BIG-IP gtm region module
+description:
+    - Configures a Global Traffic Manager region.
+version_added: 2.3
+author:
+    - "Eric Jacob, @erjac77"
+notes:
+    - Requires BIG-IP software version >= 11.6
+requirements:
+    - f5-sdk
+options:
+    description:
+        description:
+            - Specifies a user-defined description.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    name:
+        description:
+            - Specifies unique name for the component.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    partition:
+        description:
+            - Specifies the administrative partition in which the component object resides.
+        required: false
+        default: Common
+        choices: []
+        aliases: []
+        version_added: 2.3
+    state:
+        description:
+            - Specifies the state of the component on the BIG-IP system.
+        required: false
+        default: present
+        choices: ['absent', 'present']
+        aliases: []
+        version_added: 2.3
+'''
+
+EXAMPLES = '''
+- name: Create GTM Region
+  f5bigip_gtm_region:
+    f5bigip_hostname: 172.16.227.35
+    f5bigip_username: admin
+    f5bigip_password: admin
+    f5bigip_port: 443
+    name: my_region
+    partition: Common
+    description: My region
+    state: present
+  delegate_to: localhost
+'''
+
+from ansible_common_f5bigip.f5bigip import *
+
+BIGIP_GTM_REGION_ARGS = dict(
+    #app_service     =   dict(type='str'),
+    description     =   dict(type='str')#,
+    #region_members  =   dict(type='list')
+)
+
+class F5BigIpGtmRegion(F5BigIpObject):
+    def _set_crud_methods(self):
+        self.methods = {
+            'create':self.mgmt.tm.gtm.regions.region.create,
+            'read':self.mgmt.tm.gtm.regions.region.load,
+            'update':self.mgmt.tm.gtm.regions.region.update,
+            'delete':self.mgmt.tm.gtm.regions.region.delete,
+            'exists':self.mgmt.tm.gtm.regions.region.exists
+        }
+
+def main():
+    # Translation list for conflictual params
+    tr = {}
+    
+    module = AnsibleModuleF5BigIpObject(
+        argument_spec=BIGIP_GTM_REGION_ARGS,
+        supports_check_mode=False,
+        mutually_exclusive=[
+            ['disabled', 'enabled']
+        ]
+    )
+    
+    try:
+        obj = F5BigIpGtmRegion(check_mode=module.supports_check_mode, tr=tr, **module.params)
+        result = obj.flush()
+        module.exit_json(**result)
+    except Exception as exc:
+        module.fail_json(msg=str(exc))
+
+from ansible.module_utils.basic import *
+
+if __name__ == '__main__':
+    main()
