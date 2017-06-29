3012b80b4b07d0473b912c885f55c693a5b214cb
Gab Fortin <gabfortin@icloud.com>
2017-06-29 11:24:30 -0400

    Add LTM iFile support

---
+++ library/f5bigip_ltm_ifile.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+#
+# Copyright 2016, Eric Jacob <erjac77@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: f5bigip_ltm_ifile
+short_description: BIG-IP ltm ifile module
+description:
+    - Configures an iFile.
+version_added: 2.3
+author:
+	- "Gabriel Fortin"
+notes:
+    - Requires BIG-IP software version >= 11.6
+requirements:
+    - f5-sdk
+options:
+    app_service:
+        description:
+            - Specifies the application service to which the object belongs.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    description:
+        description:
+            - Specifies descriptive text that identifies the component.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    file_name:
+        description:
+            - The name of the iFile File Object that this iFile uses.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    name:
+        description:
+            - Specifies a unique name for the component.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    partition:
+        description:
+            - Specifies the administrative partition in which the component object resides.
+        required: false
+        default: Common
+        choices: []
+        aliases: []
+        version_added: 2.3
+    state:
+        description:
+            - Specifies the state of the component on the BIG-IP system.
+        required: false
+        default: present
+        choices: ['absent', 'present']
+        aliases: []
+        version_added: 2.3
+'''
+
+EXAMPLES = '''
+- name: Create LTM Ifile
+  f5bigip_ltm_ifile:
+    f5_hostname: 172.16.227.35
+    f5_username: admin
+    f5_password: admin
+    f5_port: 443
+    name: my_ifile
+    partition: Common
+    description: My ifile
+    file_name: /Common/my_ifile
+    state: present
+  delegate_to: localhost
+'''
+
+from ansible_common_f5.f5_bigip import *
+
+BIGIP_LTM_IFILE_ARGS = dict(
+    app_service     =   dict(type='str'),
+    description     =   dict(type='str'),
+    file_name       =   dict(type='str')
+)
+
+class F5BigIpLtmIfile(F5BigIpNamedObject):
+    def set_crud_methods(self):
+        self.methods = {
+            'create':   self.mgmt_root.tm.ltm.ifiles.ifile.create,
+            'read':     self.mgmt_root.tm.ltm.ifiles.ifile.load,
+            'update':   self.mgmt_root.tm.ltm.ifiles.ifile.update,
+            'delete':   self.mgmt_root.tm.ltm.ifiles.ifile.delete,
+            'exists':   self.mgmt_root.tm.ltm.ifiles.ifile.exists
+        }
+
+def main():
+    # Translation list for conflictual params
+    tr = {}
+
+    module = AnsibleModuleF5BigIpNamedObject(argument_spec=BIGIP_LTM_IFILE_ARGS, supports_check_mode=False)
+
+    try:
+        obj = F5BigIpLtmIfile(check_mode=module.supports_check_mode, tr=tr, **module.params)
+        result = obj.flush()
+        module.exit_json(**result)
+    except Exception as exc:
+        module.fail_json(msg=str(exc))
+
+from ansible.module_utils.basic import *
+
+if __name__ == '__main__':
+    main()
