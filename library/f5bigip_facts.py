4e6156c47a79e9ce74487281b247fe7099a46330
erjac77 <erjac77@gmail.com>
2017-08-01 10:17:36 -0400

    Update BIG-IP Facts module

--- library/f5bigip_facts.py
+++ library/f5bigip_facts.py
@@ -22,23 +22,63 @@ description:
     - Collect facts from BIG-IP system.
 version_added: 2.3
 author:
-	- "Eric Jacob, @erjac77"
+    - "Eric Jacob, @erjac77"
 notes:
     - Requires BIG-IP software version >= 11.6
 requirements:
     - f5-sdk
 options:
-    include:
-        description:
-            - Specifies the list of objects to collect.
-        required: true
-        default: null
-        choices: [node, vlan]
-        aliases: []
-        version_added: 2.3
     filter:
         description:
             - Specifies an administrative partition to query for a result set.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    component:
+        description:
+            - Specifies the component to collect.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    module:
+        description:
+            - Specifies the module.
+        required: true
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    select:
+        description:
+            - Specifies a subset of the properties that will appear in the result set.
+        required: false
+        default: ltm
+        choices: []
+        aliases: []
+        version_added: 2.3
+    sub_module:
+        description:
+            - Specifies the sub-module.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    skip:
+        description:
+            - Specifies the number of rows to skip in the result set.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+        version_added: 2.3
+    top:
+        description:
+            - Specifies the first N rows of the result set.
         required: false
         default: null
         choices: []
@@ -53,7 +93,10 @@ EXAMPLES = '''
     f5_username: admin
     f5_password: admin
     f5_port: 443
-    object: vlan
+    module: ltm
+    component: pool
+    filter: partition eq Common
+    select: name,partition
   delegate_to: localhost
 '''
 
@@ -63,17 +106,23 @@ def get_facts(uri, **params):
     rparams = dict()
     rq_filter = params['filter']
     rq_select = params['select']
+    rq_skip = params['skip']
+    rq_top = params['top']
     if rq_filter:
         rparams['filter'] = rq_filter.replace(" ", "+")
     if rq_select:
         if 'name' not in rq_select:
             rq_select.append('name')
         rparams['select'] = rq_select
+    if rq_skip:
+        rparams['skip'] = rq_skip
+    if rq_top:
+        rparams['top'] = rq_top
 
     req_params = ""
     if rparams:
         for k, v in rparams.iteritems():
-            req_params += "&$" + k + "=" + v
+            req_params += "&$" + k + "=" + str(v)
 
     resp = open_url(
         'https://' + params['f5_hostname'] + ':' + str(params['f5_port']) + uri + '?expandSubcollections=true' + req_params,
@@ -87,9 +136,14 @@ def get_facts(uri, **params):
 
 def main():
     argument_spec = dict(
-        object 	= 	dict(type='str', required=True, choices=['node', 'vlan']),
-        filter	=	dict(type='str'),
-        select  =	dict(type='str')
+        component   =   dict(type='str', required=True),
+        filter      =   dict(type='str'),
+        module      =   dict(type='str', required=True, choices=['actions', 'analytics', 'apm', 'asm', 'auth', 'cli', 'cm',
+            'gtm', 'ltm', 'net', 'pem', 'security', 'sys', 'transaction', 'util', 'vcmp', 'wam', 'wom']),
+        select      =   dict(type='str'),
+        sub_module  =   dict(type='str'),
+        skip        =   dict(type='int'),
+        top         =   dict(type='int')
     )
     
     module = AnsibleModuleF5BigIpUnnamedObject(argument_spec=argument_spec, supports_check_mode=False)
@@ -97,13 +151,13 @@ def main():
     try:
         facts = {}
         
-        if module.params['object'] == 'node':
-        	facts['node'] = get_facts(uri='/mgmt/tm/ltm/node/', **module.params)
-        if module.params['object'] == 'vlan':
-        	facts['vlan'] = get_facts(uri='/mgmt/tm/net/vlan/', **module.params)
-        
+        resource = module.params['module']
+        if module.params['sub_module'] is not None:
+            resource += '/' + module.params['sub_module']
+        resource += '/' + module.params['component']
+
+        facts[resource.replace('/', '_')] = get_facts(uri='/mgmt/tm/' + resource + '/', **module.params)
         result = { 'ansible_facts': facts }
-        
         module.exit_json(**result)
     except Exception as exc:
         module.fail_json(msg=str(exc))
