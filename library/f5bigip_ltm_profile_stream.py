33633fc622a992327bb703412903a9faefbd0c4a
Gab Fortin <gabfortin@icloud.com>
2017-09-29 10:27:14 -0400

    Add LTM Profile Stream support

---
+++ library/f5bigip_ltm_profile_stream.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+#
+# Copyright 2016, Eric Jacob <erjac77@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: f5bigip_ltm_profile_stream
+short_description: BIG-IP ltm profile stream module
+description:
+    - Configures a Stream profile.
+version_added: 2.3
+author:
+    - "Gabriel Fortin"
+notes:
+    - Requires BIG-IP software version >= 11.6
+requirements:
+    - f5-sdk
+options:
+    app_service:
+        description:
+            - Specifies the name of the application service to which the profile belongs.
+        required: false
+        default: none
+        choices: []
+        aliases: []
+    defaults_from:
+        description:
+            - Specifies the profile that you want to use as the parent profile.
+        required: false
+        default: stream
+        choices: []
+        aliases: []
+    description:
+        description:
+            - User defined description.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+    name:
+        description:
+            - Specifies a unique name for the component.
+        required: true
+        default: none
+        choices: []
+        aliases: []
+    partition:
+        description:
+            - Displays the administrative partition within which the component resides.
+        required: false
+        default: null
+        choices: []
+        aliases: []
+    source:
+        description:
+            - Specifies the string that you want to rewrite.
+        required: false
+        default: none
+        choices: []
+        aliases: []
+    state:
+        description:
+            - Specifies the state of the component on the BIG-IP system.
+        required: false
+        default: present
+        choices: ['absent', 'present']
+        aliases: []
+    target:
+        description:
+            - Specifies the new string, to replace the source string.
+        required: false
+        default: none
+        choices: []
+        aliases: []
+'''
+
+EXAMPLES = '''
+- name: Create LTM Profile stream
+  f5bigip_ltm_profile_stream:
+    f5_hostname: 172.16.227.35
+    f5_username: admin
+    f5_password: admin
+    f5_port: 443
+    name: my_stream_profile
+    partition: Common
+    description: My stream profile
+    state: present
+  delegate_to: localhost
+'''
+
+from ansible_common_f5.f5_bigip import *
+
+BIGIP_LTM_PROFILE_STREAM_ARGS = dict(
+    app_service      =    dict(type='str'),
+    defaults_from    =    dict(type='str'),
+    description      =    dict(type='str'),
+    source           =    dict(type='str'),
+    target           =    dict(type='str')
+)
+
+class F5BigIpLtmProfileStream(F5BigIpNamedObject):
+    def set_crud_methods(self):
+        self.methods = {
+            'create':   self.mgmt_root.tm.ltm.profile.streams.stream.create,
+            'read':     self.mgmt_root.tm.ltm.profile.streams.stream.load,
+            'update':   self.mgmt_root.tm.ltm.profile.streams.stream.update,
+            'delete':   self.mgmt_root.tm.ltm.profile.streams.stream.delete,
+            'exists':   self.mgmt_root.tm.ltm.profile.streams.stream.exists
+        }
+
+def main():
+    # Translation list for conflictual params
+    tr = {}
+
+    module = AnsibleModuleF5BigIpNamedObject(argument_spec=BIGIP_LTM_PROFILE_STREAM_ARGS, supports_check_mode=False)
+
+    try:
+        obj = F5BigIpLtmProfileStream(check_mode=module.supports_check_mode, tr=tr, **module.params)
+        result = obj.flush()
+        module.exit_json(**result)
+    except Exception as exc:
+        module.fail_json(msg=str(exc))
+
+from ansible.module_utils.basic import *
+
+if __name__ == '__main__':
+    main()
