16c284d5479ee6a3607a26729ad9acfa71d9a980
erjac77 <erjac77@gmail.com>
2017-11-17 16:04:39 -0500

    Add Auth Partition support

---
+++ library/f5bigip_auth_partition.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+#
+# Copyright 2016-2017, Eric Jacob <erjac77@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: f5bigip_auth_partition
+short_description: BIG-IP auth partition module
+description:
+    - Configures administrative partitions that implement access control for BIG-IP system users.
+version_added: "2.4"
+author:
+    - "Eric Jacob (@erjac77)"
+options:
+    default_route_domain:
+        description:
+            - Specifies the ID of the route domain that is associated with the IP addresses that reside in the partition.
+    description:
+        description:
+            - Specifies descriptive text that identifies the component.
+    name:
+        description:
+            - Specifies unique name for the component.
+        required: true
+    state:
+        description:
+            - Specifies the state of the component on the BIG-IP system.
+        default: present
+        choices: ['absent', 'present']
+notes:
+    - Requires BIG-IP software version >= 11.6
+requirements:
+    - ansible-common-f5
+    - f5-sdk
+'''
+
+EXAMPLES = '''
+- name: Create Auth Partition
+  f5bigip_auth_partition:
+    f5_hostname: 172.16.227.35
+    f5_username: admin
+    f5_password: admin
+    f5_port: 443
+    name: my_partition
+    description: My partition
+    state: present
+  delegate_to: localhost
+'''
+
+RETURN = '''
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_common_f5.f5_bigip import *
+
+BIGIP_AUTH_PARTITION_ARGS = dict(
+    default_route_domain    =   dict(type='int'),
+    description             =   dict(type='str'),
+)
+
+class F5BigIpAuthPartition(F5BigIpNamedObject):
+    def set_crud_methods(self):
+        self.methods = {
+            'create':   self.mgmt_root.tm.auth.partitions.partition.create,
+            'read':     self.mgmt_root.tm.auth.partitions.partition.load,
+            'update':   self.mgmt_root.tm.auth.partitions.partition.update,
+            'delete':   self.mgmt_root.tm.auth.partitions.partition.delete,
+            'exists':   self.mgmt_root.tm.auth.partitions.partition.exists
+        }
+        del self.params['partition']
+        del self.params['sub_path']
+
+def main():
+    module = AnsibleModuleF5BigIpNamedObject(argument_spec=BIGIP_AUTH_PARTITION_ARGS, supports_check_mode=False)
+
+    try:
+        obj = F5BigIpAuthPartition(check_mode=module.supports_check_mode, **module.params)
+        result = obj.flush()
+        module.exit_json(**result)
+    except Exception as exc:
+        module.fail_json(msg=str(exc))
+
+if __name__ == '__main__':
+    main()
