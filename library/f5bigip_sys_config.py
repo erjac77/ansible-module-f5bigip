45d79cc0212ed6bd8bce2a9ac2a86332ee18bbe5
erjac77 <erjac77@gmail.com>
2018-01-03 10:45:46 -0500

    Add options documentation for SYS Config module

--- library/f5bigip_sys_config.py
+++ library/f5bigip_sys_config.py
@@ -30,9 +30,62 @@ version_added: "2.4"
 author:
     - "Eric Jacob (@erjac77)"
 options:
-    args:
+    base:
         description:
-            - Specifies arguments of the config command.
+            - Indicates the base configuration.
+        choices: ['yes', 'no']
+    binary:
+        description:
+            - Indicates binary configuration.
+        choices: ['yes', 'no']
+    default:
+        description:
+            - Indicates factory default configuration.
+        choices: ['yes', 'no']
+    exclude_gtm:
+        description:
+            - Indicates the BIG-IP configuration, excluding GTMs.
+        choices: ['yes', 'no']
+    file:
+        description:
+            - Loads or saves a configuration from the specified file.
+    files_folder:
+        description:
+            - Loads disk files referred to by the configuration from the folder tree under the specified folder.
+    gtm_only:
+        description:
+            - Indicates the Global Traffic Manage (GTM) configuration.
+        choices: ['yes', 'no']
+    merge:
+        description:
+            - Loads the configuration from the specified file, which modifies the running configuration.
+        choices: ['yes', 'no']
+    partitions:
+        description:
+            - Indicates the partitions in which configuration components reside.
+    passphrase:
+        description:
+            - Specifies a password to save or load an encrypted configuration file.
+    user_only:
+        description:
+            - Indicates the configuration including user account information only.
+        choices: ['yes', 'no']
+    tar_file:
+        description:
+            - Loads or saves disk files referred to by the configuration from the specified tar file.
+    time_stamp:
+        description:
+            - Inserts a time-stamp in a file name.
+        choices: ['yes', 'no']
+    verify:
+        description:
+            - Validates the specified configuration from file(s) without changing the running configuration.
+        choices: ['yes', 'no']
+    wait:
+        description:
+            - Specifies that tmsh should wait for another instance of tmsh to finish saving the configuration before
+              proceeding.
+        choices: ['yes', 'no']
 notes:
     - Requires BIG-IP software version >= 11.6
 requirements:
@@ -47,7 +100,7 @@ EXAMPLES = '''
     f5_username: admin
     f5_password: admin
     f5_port: 443
-    command: 'save'
+    command: save
   delegate_to: localhost
 
 - name: Load/Merge SYS Config
@@ -56,8 +109,8 @@ EXAMPLES = '''
     f5_username: admin
     f5_password: admin
     f5_port: 443
-    command: 'load'
-    merge: True
+    command: load
+    merge: yes
     file: my_file
   delegate_to: localhost
 '''
@@ -69,23 +122,27 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible_common_f5.f5_bigip import *
 
 BIGIP_SYS_CONFIG_ARGS = dict(
-    command=dict(type='str', choices=['save', 'load']),
-    base=dict(type='str', choices=['binary', 'default', 'gtm-only', 'user-only']),
-    binary=dict(type='bool'),
+    # Command
+    command=dict(type='str', choices=['save', 'load'], required=True),
+    # Common arguments
+    base=dict(type='bool'),
     current_partition=dict(type='bool'),
-    default=dict(type='bool'),
     exclude_gtm=dict(type='bool'),
     file=dict(type='str'),
-    files_folder=dict(type='str'),
     gtm_only=dict(type='bool'),
-    merge=dict(type='bool'),
     partitions=dict(type='list'),
     passphrase=dict(type='str', no_log=True),
     user_only=dict(type='bool'),
     tar_file=dict(type='str'),
+    # Save specific arguments
+    binary=dict(type='bool'),
     time_stamp=dict(type='bool'),
-    verify=dict(type='bool'),
-    wait=dict(type='bool')
+    wait=dict(type='bool'),
+    # Load specific arguments
+    default=dict(type='bool'),
+    files_folder=dict(type='str'),
+    merge=dict(type='bool'),
+    verify=dict(type='bool')
 )
 
 
@@ -105,14 +162,20 @@ class F5BigIpSysConfig(F5BigIpUnnamedObject):
         try:
             self.methods['exec_cmd'](command, **params)
             has_changed = True
-        except Exception as e:
-            raise AnsibleF5Error("Could not execute '" + command + "' command: " + e.message)
+        except Exception as exc:
+            raise AnsibleF5Error("Could not execute '" + command + "' command: " + exc.message)
 
         return {'changed': has_changed}
 
 
 def main():
-    module = AnsibleModuleF5BigIpUnnamedObject(argument_spec=BIGIP_SYS_CONFIG_ARGS, supports_check_mode=False)
+    module = AnsibleModuleF5BigIpUnnamedObject(
+        argument_spec=BIGIP_SYS_CONFIG_ARGS,
+        supports_check_mode=False,
+        mutually_exclusive=[
+            ['base', 'binary', 'default', 'exclude_gtm', 'gtm_only', 'user_only']
+        ]
+    )
 
     try:
         obj = F5BigIpSysConfig(check_mode=module.supports_check_mode, **module.params)
